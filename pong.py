8ea676747b496de9bbfab7642e2f5124f7d53ab2
Petr Verner <petr.verner1864@gmail.com>
2022-06-11 22:36:20 +0200

    add the second part

--- pong.py
+++ pong.py
@@ -1,10 +1,15 @@
 import turtle
+import os
 
 wn = turtle.Screen()
 wn.title("Pong")
 wn.bgcolor("#008000")
 wn.setup(width=800, height=600, startx=True, starty=True)
 wn.tracer(0, 1)
+
+# setup
+score_a = 0
+score_b = 0
 
 # Federer
 federer = turtle.Turtle()
@@ -67,3 +72,62 @@ wn.onkeypress(federer_down, "Down")
 while True:
     wn.update()
     wn.listen(30, 40)
+
+    # move the ball and the Nadal
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+    nadal.sety(nadal.ycor() + nadal.dy)
+
+    # border checking for the ball and the Nadal
+
+    # top and bottom
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        os.system("aplay bounce.wav&")
+
+    elif ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        os.system("aplay bounce.wav&")
+
+    if nadal.ycor() > 290:
+        nadal.sety(290)
+        nadal.dy *= -1
+
+    elif nadal.ycor() < -290:
+        nadal.sety(-290)
+        nadal.dy *= -1
+
+    # left and right
+    if ball.xcor() > 350:
+        score_a += 1
+        pen.clear()
+        pen.write("Roger Federer: {} Rafael Nadal: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        ball.goto(0, 0)
+        ball.dx *= -1
+        os.system("aplay bounce.wav&")
+
+    elif ball.xcor() < -350:
+        score_b += 1
+        pen.clear()
+        pen.write("Roger Federer: {} Rafael Nadal: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        ball.goto(0, 0)
+        ball.dx *= -1
+        os.system("aplay bounce.wav&")
+
+    # border checking for the Federer
+    if federer.ycor() > 260:
+        federer.sety(260)
+
+    if federer.ycor() < -260:
+        federer.sety(-260)
+
+    # paddle and ball collistions
+    if ball.xcor() < -340 and ball.ycor() < federer.ycor() + 40 and ball.ycor() > federer.ycor() - 40:
+        ball.dx *= -1
+        os.system("aplay punch.wav&")
+
+    elif ball.xcor() > 340 and ball.ycor() < nadal.ycor() + 40 and ball.ycor() > nadal.ycor() - 40:
+        ball.dx *= -1
+        os.system("aplay boom.wav&")
